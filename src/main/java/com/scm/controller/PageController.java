dcdbb67bfc749ef12a30e42ca56f4b1866dda12a
Ansh255 <anshgandhi.21.cs@iite.indusuni.ac.in>
2024-07-14 20:13:45 +0530

    test

--- src/main/java/com/scm/controller/PageController.java
+++ src/main/java/com/scm/controller/PageController.java
@@ -7,22 +7,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
     @RequestMapping("/home")
-    public String home(Model model){
-        System.out.println("Home page handler ");
-        model.addAttribute("name","substring");
-        model.addAttribute("Youtube","AnshGandhi");
+    public String home(Model model) {
+        System.out.println("Home page handler");
+        // sending data to view
+        model.addAttribute("name", "Substring Technologies");
+        model.addAttribute("youtubeChannel", "Learn Code With Durgesh");
+        model.addAttribute("githubRepo", "https://github.com/learncodewithdurgesh/");
         return "home";
     }
 
-    //route pages
+    // about route
+
     @RequestMapping("/about")
-    public String about(){
+    public String aboutPage(Model model) {
+        model.addAttribute("isLogin", true);
+        System.out.println("About page loading");
         return "about";
     }
 
-    //services
+    // services
+
     @RequestMapping("/services")
-    public String service(){
+    public String servicesPage() {
+        System.out.println("services page loading");
         return "services";
     }
+
 }
