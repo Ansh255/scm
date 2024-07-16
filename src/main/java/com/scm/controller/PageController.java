ddcd81f3c8d73e0b285177531788bf50693cd9e7
Ansh255 <anshgandhi.21.cs@iite.indusuni.ac.in>
2024-07-16 17:34:29 +0530

    test

--- src/main/java/com/scm/controller/PageController.java
+++ src/main/java/com/scm/controller/PageController.java
@@ -2,7 +2,9 @@ package com.scm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
@@ -15,22 +17,17 @@ public class PageController {
         model.addAttribute("githubRepo", "https://github.com/learncodewithdurgesh/");
         return "home";
     }
-
     // about route
-
     @RequestMapping("/about")
     public String aboutPage(Model model) {
         model.addAttribute("isLogin", true);
         System.out.println("About page loading");
         return "about";
     }
-
     // services
-
     @RequestMapping("/services")
     public String servicesPage() {
         System.out.println("services page loading");
         return "services";
     }
-
 }
